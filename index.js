eee06c1bc59e66109e4a5d4567cc7da4a132a9af
oa-dev <arroyo.ot@gmail.com>
2021-07-12 22:10:58 +0100

    Get all users by name

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const data = require("../data/fake-users/users-posts.json");
 
 const getAllVehicles = () => data.map((profile) => profile.vehicle);
+
+const getAllUsers = () =>
+  data.map((profile) => `${profile.firstName} ${profile.lastName}`);
