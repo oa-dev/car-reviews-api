e24605d92f52f538be9f7d4d8fe88a64b3f45e60
oa-dev <arroyo.ot@gmail.com>
2021-07-13 12:03:44 +0100

    Handle 404 response with middleware

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
+app.use((req, res, next) => {
+  res.status(404).send("404 Not Found");
+});
+
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
