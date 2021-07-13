c6cba1a1d0dba7c8427de3a5854332d5d4877a74
oa-dev <arroyo.ot@gmail.com>
2021-07-13 14:09:26 +0100

    Provide comments for functions

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const helmet = require("helmet");
-const app = express();
-const port = process.PORT || 3000;
 const { getAllVehicles, getAllUsers } = require("./utils/helpers");
 
+const app = express();
+const port = process.PORT || 3000;
+
+// middleware
 app.use(helmet());
 
+// routes
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
+// all other route handling
 app.use((req, res, next) => {
   res.status(404).send("404 Not Found");
 });
 
+// error handling
 app.use((err, req, res, next) => {
   res.status(500).render("error", { error: err });
 });
