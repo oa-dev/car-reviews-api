4a47b0b9be7c43fe207b18ed84815b55324359d3
oa-dev <arroyo.ot@gmail.com>
2021-07-13 13:58:56 +0100

    Add a catch all error handler

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const port = process.PORT || 3000;
 const { getAllVehicles, getAllUsers } = require("./utils/helpers");
+
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("hello world!");
@@ -9,6 +12,10 @@ app.get("/", (req, res) => {
 
 app.use((req, res, next) => {
   res.status(404).send("404 Not Found");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).render("error", { error: err });
 });
 
 app.listen(port, () => {
